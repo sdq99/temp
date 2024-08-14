8f6b71e9ea7e7be0074a75b7b832c0ec5b28d209
MD Sadique Imam <35928705+sdq99@users.noreply.github.com>
2024-08-14 22:26:28 +0530

    Update build_amd64.sh

--- build_amd64.sh
+++ build_amd64.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64 -t d . && docker run --platform linux/amd64 d
+docker buildx build --platform linux/amd64 -t ddd:latest . && docker run --platform linux/amd64 ddd:latest
